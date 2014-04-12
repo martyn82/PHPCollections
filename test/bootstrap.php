697c5f89c2bdaa3e40ebf5b013865304ec296706
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-04-12 11:48:39 +0200

    Using composer for dependency management and autoload

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,15 +1,3 @@
 <?php
-$testPath = realpath( __DIR__ );
-$sourcePath = realpath( __DIR__ . DIRECTORY_SEPARATOR . '../src' );
-
-spl_autoload_register( function ( $className ) use ( $testPath, $sourcePath ) {
-	$fileName = str_replace( '\\', DIRECTORY_SEPARATOR, $className ) . '.php';
-
-	$paths = array( $testPath, $sourcePath );
-
-	foreach ( $paths as $path ) {
-		@include_once $path . DIRECTORY_SEPARATOR . $fileName;
-	}
-} );
-
-require_once 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
+require_once 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once 'TestCase.php';
