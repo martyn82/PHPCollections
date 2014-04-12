c7ca6f90dd6d19bf4e4ded40ec0a02930931d655
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-04-12 16:51:27 +0200

    Implemented methods for EntrySet to allow better checking of complex objects

--- main/src/Collections/Map/EntrySet.php
+++ main/src/Collections/Map/EntrySet.php
@@ -21,4 +21,56 @@ class EntrySet extends AbstractSet {
 	private function _add( Entry $element ) {
 		return parent::add( $element );
 	}
+
+	/**
+	 * @see AbstractCollection::remove()
+	 */
+	public function remove( $element ) {
+		return $this->_remove( $element );
+	}
+
+	/**
+	 * Type-safe remove.
+	 *
+	 * @param Entry $element
+	 *
+	 * @return boolean
+	 */
+	private function _remove( Entry $element ) {
+		foreach ( $this->elements as $index => $entry ) {
+			/* @var $entry Entry */
+			if ( $entry->getKey() === $element->getKey() ) {
+				unset( $this->elements[ $index ] );
+				$this->elements = \array_values( $this->elements );
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @see AbstractCollection::contains()
+	 */
+	public function contains( $element ) {
+		return $this->_contains( $element );
+	}
+
+	/**
+	 * Type-safe contains.
+	 *
+	 * @param Entry $element
+	 *
+	 * @return boolean
+	 */
+	private function _contains( Entry $element ) {
+		foreach ( $this->elements as $entry ) {
+			/* @var $entry Entry */
+			if ( $entry->getKey() === $element->getKey() ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
